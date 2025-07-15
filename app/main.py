04aec4640f77ceadbe65300933a0b9834ff74451
ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>
2025-07-15 11:06:50 +0530

    add cors in main.py
    Signed-off-by: ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, APIRouter
 from api import org_router,user_router, auth_router, doc_router
 from db import get_database, close_db_connection
 from pymongo.asynchronous.database import AsyncDatabase
 from core import AppBaseException, app_base_exception_handler, DatabaseConnectionException
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
 
+
+origins = [
+    "http://localhost:3000",         # React dev server
+    "http://127.0.0.1:3000"
+]
+
 app.add_exception_handler(AppBaseException, app_base_exception_handler)
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,              # or ["*"] to allow all (dev only)
+    allow_credentials=True,
+    allow_methods=["*"],                # allow all HTTP methods
+    allow_headers=["*"],                # allow all headers
+)
+
+api_router = APIRouter(prefix="/api")
 @app.on_event("startup")
 async def startup_event():
     """
@@ -35,7 +54,9 @@ async def health_check(db:AsyncDatabase = Depends(get_database)):
     finally:
         await close_db_connection()
 
-app.include_router(org_router, prefix='/organization' ,tags=["Organization"])
-app.include_router(user_router, prefix='/user' ,tags=["Users"])
-app.include_router(auth_router, prefix="/auth" ,tags=["Authentication"])
-app.include_router(doc_router, prefix="/doc", tags=["Docs"])
+api_router.include_router(org_router, prefix='/organization' ,tags=["Organization"])
+api_router.include_router(user_router, prefix='/user' ,tags=["Users"])
+api_router.include_router(auth_router, prefix="/auth" ,tags=["Authentication"])
+api_router.include_router(doc_router, prefix="/doc", tags=["Docs"])
+
+app.include_router(api_router)
